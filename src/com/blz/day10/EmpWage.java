1462372886caa2a404f082cc4d6fcbda825d1502
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-02 22:04:25 +0530

    Added : Ability to add to save total Wage

--- src/com/blz/day10/EmpWage.java
+++ src/com/blz/day10/EmpWage.java
@@ -2,54 +2,66 @@ package com.blz.day10;
 
 import java.util.Random;
 
-class utilityEmpWage {
-	Random rand = new Random();
-	public final int isPresent = 1;
-	public final int isAbsent = 2;
-	public final int isPartTime = 3;
-	public int wagePerHr = 20;
-	public int fullDayHr = 8;
-	public int partTimeHr = 4;
-	public int maxWorkingDays = 20;
-	public int workingDays = 0;
-	public int maxWorkingHr = 100;
-	public int workingHr = 0;
-	public int dailyWage;
-	public int hrs = 0;
-
-	public void present(String name, int wagePerHr, int maxWorkingDays, int maxWorkingHr) {
-		while (workingHr < maxWorkingHr && workingDays < maxWorkingDays) {
-			int check = rand.nextInt(3) + 1;
-			switch (check) {
-			case isPresent -> {
-				System.out.println("Employee is present.");
-				hrs = fullDayHr;
-			}
-			case isPartTime -> {
-				System.out.println("Employee is present part time.");
-				hrs = partTimeHr;
-			}
-			case isAbsent -> hrs = 0;
-			}
-			workingHr = workingHr + hrs;
-			workingDays = workingDays + 1;
-		}
-		int salary = workingHr * wagePerHr;
-		System.out.println("the salary of month is of " + name + " employee is " + salary);
-	}
-}
-
 public class EmpWage {
+
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxWorkingHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxWorkingHoursPerMonth = maxWorkingHoursPerMonth;
+	}
+
+	public void computeWage() {
+		int empHours = 0;
+		int total_empHrs = 0;
+		int totalWorkingDay = 0;
+
+		while (total_empHrs <= maxWorkingHoursPerMonth && totalWorkingDay < numOfWorkingDays) {
+			totalWorkingDay++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHours = 8;
+				System.out.println("Employee is Present");
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee is Part Time");
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
+
+				System.out.println("Employee is Absent");
+			}
+			total_empHrs += empHours;
+		}
+		totalEmpWage = total_empHrs * empRatePerHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp wage for company : " + company + " is " + totalEmpWage;
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome To Employee Wage Computation");
-		utilityEmpWage bridgrLabz = new utilityEmpWage();
-		utilityEmpWage airtel = new utilityEmpWage();
-		System.out.println("For bridgeLabz employee : ");
-		bridgrLabz.present("bridgeLabz", 20, 25, 180);
-		System.out.println("For airtel employee : ");
-		airtel.present("airtel", 25, 22, 150);
+		
+		 EmpWage bridgeLabz = new EmpWage("BridgeLabz", 45, 30, 70);
+		 EmpWage infosys = new EmpWage("Infosys", 35, 45, 90);
+         bridgeLabz.computeWage();
+         System.out.println(bridgeLabz);
+         infosys.computeWage();
+         System.out.println(infosys);
+
 	}
 
 }
