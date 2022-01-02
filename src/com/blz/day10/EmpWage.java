0dffb1649f19ad0a2376699839540117931790e9
Sandipan Kanade <94506572+sk09822@users.noreply.github.com>
2022-01-02 21:21:13 +0530

    Update EmpWage.java

--- src/com/blz/day10/EmpWage.java
+++ src/com/blz/day10/EmpWage.java
@@ -6,13 +6,24 @@ class utilityEmpWage {
 	Random rand = new Random();
 	public int isPresent = 1;
 	public int isAbsent = 2;
+	public int wagePerHr = 20;
+	public int fullDayHr = 8;
+	int check = rand.nextInt(2) + 1;
 
-	public void present() {
-		int check = rand.nextInt(2) + 1;
+	public boolean present() {
 		if (check == isAbsent) {
 			System.out.println("Employee is absent.");
+			return false;
 		} else {
 			System.out.println("Employee is present.");
+			return true;
+		}
+	}
+
+	public void dailyWage() {
+		if (present()) {
+			int dailyWage = wagePerHr * fullDayHr;
+			System.out.println("Wage of the day of employee is : " + dailyWage);
 		}
 	}
 }
@@ -23,6 +34,7 @@ public class EmpWage {
 		System.out.println("Welcome To Employee Wage Computation");
 		utilityEmpWage empWage = new utilityEmpWage();
 		empWage.present();
+		empWage.dailyWage();
 	}
 
 }
